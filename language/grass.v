e87f23fe1202303f997e3e5947a9cc9a4c823050
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2021-01-22 17:26:23 +0900

    Update grass.v

--- language/grass.v
+++ language/grass.v
@@ -64,20 +64,35 @@ End machine.
 Section operational_semantics.
 
 Inductive has_value : stack -> env -> Prop :=
-| HALT : forall E : env , has_value (Stk Cnil E Dnil) E
+| HALT : forall (E : env) , has_value (Stk Cnil E Dnil) E
 | step : forall (S S': stack) (E : env),
     Some S = one_step S' -> has_value S E -> has_value S' E.
+
 
 Lemma L0:forall (E1 E2 : env), has_value (Stk Cnil E1 Dnil) E2 -> E1 = E2.
 Proof.
   intros E1 E2 H.
+  remember (Stk Cnil E1 Dnil) as S.
   induction H.
+  injection HeqS; auto.
+  subst. simpl in H. destruct E1; inversion H.
+Qed.
+
+Lemma L1:forall (S:stack)(E:env), ~ Some S = one_step (Stk Cnil E Dnil).
+intros. unfold not. intro H. induction E;simpl in H;discriminate. Qed.
 
 Theorem partiality : forall (S:stack) (E1 E2:env),
   has_value S E1 /\ has_value S E2 -> E1 = E2.
 Proof.
   intros S E1 E2 H.
   induction H as [H0 H1].
-  induction H0. 
+  induction H0. apply L0. exact H1.
+  induction H1. apply L1 in H. apply False_ind. exact H.
+  rewrite <- H1 in H. inversion H. subst. apply IHhas_value. apply H2.
+Qed.
+
+End operational_semantics.
 
 End semantic.
+
+End grass'.
